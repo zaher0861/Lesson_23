7d3f869a4c20301539b7c123db449705bd425264
Александр Захаров <zaher0861@yandex.ru>
2022-02-10 00:08:16 +0300

    Добавьте файлы проекта.

---
+++ Lesson_23/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAsync();
+            Console.ReadKey();
+        }
+        static int Faktorial(object z)
+        {
+            int n = (int)z;
+            int[] numArray = new int[n];
+            numArray[0] = 1;
+            int num = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (i>0)
+                {
+                    numArray[i]= numArray[i-1]+1;
+                    num = num * numArray[i];
+                }
+            }
+            return num;
+        }
+        static async Task PrintAsync()
+        {
+            Console.Write("Введите любое целое число: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int result = await Task.Run(() => Faktorial(z));
+            Console.WriteLine($"Факториал данного числа = {result}"); ;
+        }
+    }
+}
